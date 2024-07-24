25151354a5a63f1e93419f1ec6bf769723571d41
Giovanna Andreoli <gibsandreoli.GA@gmail.com>
2024-07-24 14:14:49 -0300

    added main menu

---
+++ Assets/_Project/Menus/Script/MainMenuController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public string gameScene;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit");
+            Application.Quit();
+    }
+}
